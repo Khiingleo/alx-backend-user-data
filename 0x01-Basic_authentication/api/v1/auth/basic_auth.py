e4a633aeaefa9ede6e10bfbd78bddea36bedf172
shalom ogoziem <shalomzymike@gmail.com>
2024-02-13 15:36:46 +0100

    base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 """defines a class BasicAuth"""
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
     """
     BasicAuth class that inherits from Auth
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        returns the Base64 part of the Authorization header forr
+        a basic authentication
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if authorization_header[:6] == "Basic ":
+            return authorization_header[6:]
+
+        return None
