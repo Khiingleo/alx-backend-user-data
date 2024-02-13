b1c4258bbae89b99ac9982921d5e1a46629601c5
shalom ogoziem <shalomzymike@gmail.com>
2024-02-13 14:11:32 +0100

    auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" defines the class Auth """
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ class that manages API authentication """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        returns false (for now)
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        returns None (for now)
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        returns None (for now)
+        """
+        return None
