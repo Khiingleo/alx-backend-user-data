2fd52e3212ce61cdff8ae468e7d9461f5eff941e
shalom ogoziem <shalomzymike@gmail.com>
2024-02-15 10:44:52 +0100

    user id for session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ defines a class SessionAuth
 """
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -20,3 +21,10 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[id] = user_id
 
         return id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns a User id based on a session ID """
+        if session_id is None or type(session_id) is not str:
+            return None
+        value = self.user_id_by_session_id.get(session_id)
+        return value
