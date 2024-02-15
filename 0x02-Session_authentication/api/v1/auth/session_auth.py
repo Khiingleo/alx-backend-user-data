4bb4f2e14eb411e102d5f3a99c467bf2fbc710f3
shalom ogoziem <shalomzymike@gmail.com>
2024-02-15 10:26:05 +0100

    empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+defines a class SessionAuth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ session authentication class"""
+    pass # empty for now
