21522d860caabff93bb4f55c9e0c3dbeaa320b58
shalom ogoziem <shalomzymike@gmail.com>
2024-02-21 12:37:06 +0100

    log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 basic flask app
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -49,6 +49,21 @@ def login():
     result.set_cookie('session_id', session_id)
 
     return result
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout():
+    """
+    logout function to respond to the '/sessions' route
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        AUTH.destroy_session(user.id)
+        redirect("/")
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
