6587946c1c3cd073364a640c848e4a6c3bfd07ac
shalom ogoziem <shalomzymike@gmail.com>
2024-02-21 13:13:10 +0100

    update password end point

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -114,3 +114,13 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """reset the password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed,
+                                 reset_token=None)
+        except NoResultFound:
+            raise ValueError
